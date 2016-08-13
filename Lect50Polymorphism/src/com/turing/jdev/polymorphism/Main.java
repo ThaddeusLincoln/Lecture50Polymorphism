f35b321de600f8a9659624b003357004eef26f9c
ThaddeusLincolnStevens <beny1419@yahoo.es>
2016-08-13 14:01:04 +0200

    If we have a class that inherits a method, that method can be overriden and the class will have its unique functionality

--- Lect50Polymorphism/src/com/turing/jdev/polymorphism/Main.java
+++ Lect50Polymorphism/src/com/turing/jdev/polymorphism/Main.java
@@ -4,13 +4,11 @@ public class Main {
 	
 	public static void main(String[] args){
 		
-		for(int i = 1; 1 < 11; i++){
+		for(int i = 1; i < 11; i++){
 			
 			Movie movie = randomMovie();
-			System.out.println("Movie #" + i + " : " + movie.getName());
-			
+			System.out.println("Movie #" + i + " : " + movie.getName() + "\n" + "Plot: " + movie.plot());
 		}
-		
 	}
 	
 	// generates a random movie
